0734e5c369802bfe8ca2b0c184bb55abb4990d72
danis <danis@LAPTOP-3V35OI59>
2018-03-04 17:46:06 -0500

    Versión sin main

--- ParkingProject/src/parkingproject/ParkingProject.java
+++ ParkingProject/src/parkingproject/ParkingProject.java
@@ -15,6 +15,40 @@ public class ParkingProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        String greeting;
+        Celador miCelador=new Celador();
+        Dueño Dueño1 = new Dueño();
+        Dueño Dueño2 = new Dueño();
+        Dueño Dueño3 = new Dueño();
+        Dueño Dueño4 = new Dueño();
+        
+        Dueño1.alcoholLevel=0;
+        Dueño1.genreK="Mujer";
+        greeting=miCelador.greeting("Mujer");
+        System.out.println(greeting);
+        
+      
+        Dueño1.alcoholLevel=2;
+        Dueño1.genreK="Mujer";
+        greeting=miCelador.greeting("Mujer");
+        System.out.println(greeting);
+        greeting=miCelador.greeting(true);
+        System.out.println(greeting);
+        
+        Dueño2.alcoholLevel=0;
+        Dueño2.genreK="Hombre";
+        greeting=miCelador.greeting("Hombre");
+        System.out.println(greeting);
+        
+        Dueño3.alcoholLevel=2;
+        Dueño3.genreK="Hombre";
+        greeting=miCelador.greeting("Hombre");
+        System.out.println(greeting);
+        greeting=miCelador.greeting(true);
+        System.out.println(greeting);
+        
+      
+    
         // TODO code application logic here
     }
     
